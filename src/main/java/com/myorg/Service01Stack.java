f99b778752d4c39d693a691ac673450bb87c7e4f
Gui <guilherme.rddev@gmail.com>
2022-06-02 14:11:23 -0300

    feat: adiciona TargetGruop para monitorar nosso app

--- src/main/java/com/myorg/Service01Stack.java
+++ src/main/java/com/myorg/Service01Stack.java
@@ -3,12 +3,10 @@ package com.myorg;
 import software.amazon.awscdk.RemovalPolicy;
 import software.amazon.awscdk.Stack;
 import software.amazon.awscdk.StackProps;
-import software.amazon.awscdk.services.ecs.AwsLogDriverProps;
-import software.amazon.awscdk.services.ecs.Cluster;
-import software.amazon.awscdk.services.ecs.ContainerImage;
-import software.amazon.awscdk.services.ecs.LogDriver;
+import software.amazon.awscdk.services.ecs.*;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
 import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
 import software.amazon.awscdk.services.logs.LogGroup;
 import software.constructs.Construct;
 
@@ -41,5 +39,11 @@ public class Service01Stack extends Stack {
             .build())
         .publicLoadBalancer(true)
         .build();
+
+    service01.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+            .path("/actuator/health")
+            .port("8080")
+            .healthyHttpCodes("200")
+        .build());
   }
 }
