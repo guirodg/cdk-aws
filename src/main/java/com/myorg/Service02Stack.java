9c3c473cc9173b9beb0420a3c66dde1812df69b8
Gui <guilherme.rddev@gmail.com>
2022-06-03 15:39:58 -0300

    feat: adiciona service2

---
+++ src/main/java/com/myorg/Service02Stack.java
@@ -0,0 +1,68 @@
+package com.myorg;
+
+import software.amazon.awscdk.Duration;
+import software.amazon.awscdk.RemovalPolicy;
+import software.amazon.awscdk.Stack;
+import software.amazon.awscdk.StackProps;
+import software.amazon.awscdk.services.applicationautoscaling.EnableScalingProps;
+import software.amazon.awscdk.services.ecs.*;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedFargateService;
+import software.amazon.awscdk.services.ecs.patterns.ApplicationLoadBalancedTaskImageOptions;
+import software.amazon.awscdk.services.elasticloadbalancingv2.HealthCheck;
+import software.amazon.awscdk.services.logs.LogGroup;
+import software.constructs.Construct;
+
+import java.util.HashMap;
+
+public class Service02Stack extends Stack {
+  public Service02Stack(final Construct scope, final String id, Cluster cluster) {
+    this(scope, id, null, cluster);
+  }
+
+  public Service02Stack(final Construct scope, final String id, final StackProps props, final Cluster cluster) {
+    super(scope, id, props);
+
+    final HashMap<String, String> env = new HashMap<>();
+    env.put("AWS_REGION", "us-east-1");
+
+    final ApplicationLoadBalancedFargateService service02 = ApplicationLoadBalancedFargateService.Builder.create(this, "ALB02")
+        .serviceName("service02")
+        .cluster(cluster)
+        .cpu(512)
+        .desiredCount(2)
+        .listenerPort(8080)
+        .memoryLimitMiB(1024)
+        .taskImageOptions(ApplicationLoadBalancedTaskImageOptions.builder()
+            .containerName("aws_project02")
+            .image(ContainerImage.fromRegistry("guirodg/aws-local-02:1.0.0"))
+            .containerPort(8080)
+            .logDriver(LogDriver.awsLogs(AwsLogDriverProps.builder()
+                .logGroup(LogGroup.Builder.create(this, "Service02Group")
+                    .logGroupName("Service02")
+                    .removalPolicy(RemovalPolicy.DESTROY)
+                    .build())
+                .streamPrefix("Service02")
+                .build()))
+            .environment(env)
+            .build())
+        .publicLoadBalancer(true)
+        .build();
+
+    service02.getTargetGroup().configureHealthCheck(new HealthCheck.Builder()
+        .path("/actuator/health")
+        .port("8080")
+        .healthyHttpCodes("200")
+        .build());
+
+    final ScalableTaskCount scalableTaskCount = service02.getService().autoScaleTaskCount(EnableScalingProps.builder()
+        .minCapacity(2) // Minimo 2 instancias
+        .maxCapacity(4) // Maximo 4 instancias
+        .build());
+
+    scalableTaskCount.scaleOnCpuUtilization("Service02AutoScaling", CpuUtilizationScalingProps.builder()
+        .targetUtilizationPercent(50) // Minha CPU ultrapassar 50% Cria outra instancia
+        .scaleInCooldown(Duration.seconds(60)) // Tempo limite para criar nova instancia
+        .scaleOutCooldown(Duration.seconds(60)) // Tempo limite para desligar instancia
+        .build());
+  }
+}
